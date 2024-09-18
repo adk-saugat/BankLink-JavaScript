46ce147b92161f9840ee5ae978d84962104ba981
adk-saugat <adhikarisaugat34@gmail.com>
2024-09-18 14:21:10 -0500

    added login features

--- banklink-login.js
+++ banklink-login.js
@@ -1,2 +1,23 @@
 const users = getStoredUsers()
 
+document.querySelector('#loginContainer').addEventListener('submit', function (e) {
+    e.preventDefault()
+    username = e.target.elements.loginUsernameField.value
+    password = e.target.elements.loginPasswordField.value
+
+    // console.log(username)
+    // console.log(password)
+
+    const existingUser = findUser()
+
+    if (existingUser) {
+        location.assign('/dashboard.html')
+    } else {
+        e.target.elements.loginUsernameField.value = ''
+        e.target.elements.loginPasswordField.value = ''
+        //console.log('Your username and password does not match!')
+        const loginFailed = document.createElement('div')
+        loginFailed.textContent = 'Your username and password does not match. Try Again!'
+        document.querySelector('#failedWrapper').appendChild(loginFailed)
+    }
+})

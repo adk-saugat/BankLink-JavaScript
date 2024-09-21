e66accb272f68a01604e68aedd2a631ef5b8c98c
adk-saugat <adhikarisaugat34@gmail.com>
2024-09-21 15:56:01 -0500

    added link to login page

--- banklink-signup.js
+++ banklink-signup.js
@@ -7,28 +7,21 @@ document.querySelector('#signupWrapper').addEventListener('submit', function (e)
     password = e.target.elements.signupPasswordField.value
 
     // Check if the username already exists
-    const existingUser = users.find(function (user) {
-        return user.username === username && user.password === password;
-    });
-
+    const existingUser = findUser()
     if (existingUser) {
         console.log('You already have an account. No need to sign up again.');
 
         const returnWrapper = document.createElement('div')
         const loginReturn = document.createElement('span')
-        const loginLink = document.createElement('a')
 
-        loginReturn.textContent = 'You already have an account.'
-        loginLink.textContent = 'Go to Login.'
-        loginLink.setAttribute('href', '/login-page.html')
+        loginReturn.textContent = 'You already have an account. Go to Login!'
 
         returnWrapper.appendChild(loginReturn)
-        returnWrapper.appendChild(loginLink)
-
         document.querySelector('#loginReturn').appendChild(returnWrapper)
     } else {
         addUser(username, password);
         console.log('Signed up successfully.');
+        location.assign('/login-page.html')
     }
 })
 

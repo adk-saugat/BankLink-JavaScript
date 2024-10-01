d61186cd194df57a23b3bbe08b257c8363068d30
adk-saugat <adhikarisaugat34@gmail.com>
2024-09-30 22:41:15 -0500

    adding account number to each user

--- banklink-functions.js
+++ banklink-functions.js
@@ -12,6 +12,7 @@ const addUser = function (username, password) {
     let userID = crypto.randomUUID()
     users.push({
         userID: userID,
+        accountNumber: generateAccountNumber(),
         username: username,
         password: password,
         balance: 0,
@@ -26,4 +27,10 @@ const findUser = function (username, password) {
 
 const getUserID = (existingUser) => {
     return users.find((user) => user.username === existingUser.username) ? existingUser.userID : ''
+}
+
+function generateAccountNumber() {
+    // Generate a random number between 1e9 and 1e10-1 to ensure it's 10 digits
+    let randomNumber = Math.floor(Math.random() * 900000) + 100000;
+    return randomNumber;
 }

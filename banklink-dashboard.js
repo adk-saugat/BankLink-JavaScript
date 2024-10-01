e29bb7129764336f36d50ab26b7d1be74445bfed
adk-saugat <adhikarisaugat34@gmail.com>
2024-09-30 22:40:43 -0500

    managing transfer money feature

--- banklink-dashboard.js
+++ banklink-dashboard.js
@@ -1,2 +1,15 @@
-console.log(location.hash)
+const users = getStoredUsers()
+
+const user = users.find((user) => user.userID === location.hash.substring(1))
+if (user) {
+    document.querySelector('#accountBalance').textContent = `$${user.balance}`
+}
+
+document.querySelector('#transferBtn').addEventListener('click', (e) => {
+    const container = document.querySelector('#transferField')
+    const accountInfo = document.createElement('input')
+    accountInfo.setAttribute('type', 'text')
+
+    container.appendChild(accountInfo)
+})
 
